f6abc8f4d87881cfcc69f8522f1727326e537cde
bajami <bajami@simscale.com>
2024-09-16 16:47:29 +0200

    Release 0.19.3

--- simscale_sdk/models/material_update_operation_reference.py
+++ simscale_sdk/models/material_update_operation_reference.py
@@ -32,15 +32,17 @@ class MaterialUpdateOperationReference(object):
     """
     openapi_types = {
         'material_group_id': 'str',
-        'material_id': 'str'
+        'material_id': 'str',
+        'interpolation_parameters': 'dict(str, object)'
     }
 
     attribute_map = {
         'material_group_id': 'materialGroupId',
-        'material_id': 'materialId'
+        'material_id': 'materialId',
+        'interpolation_parameters': 'interpolationParameters'
     }
 
-    def __init__(self, material_group_id=None, material_id=None, local_vars_configuration=None):  # noqa: E501
+    def __init__(self, material_group_id=None, material_id=None, interpolation_parameters=None, local_vars_configuration=None):  # noqa: E501
         """MaterialUpdateOperationReference - a model defined in OpenAPI"""  # noqa: E501
         if local_vars_configuration is None:
             local_vars_configuration = Configuration()
@@ -48,12 +50,15 @@ class MaterialUpdateOperationReference(object):
 
         self._material_group_id = None
         self._material_id = None
+        self._interpolation_parameters = None
         self.discriminator = None
 
         if material_group_id is not None:
             self.material_group_id = material_group_id
         if material_id is not None:
             self.material_id = material_id
+        if interpolation_parameters is not None:
+            self.interpolation_parameters = interpolation_parameters
 
     @property
     def material_group_id(self):
@@ -100,6 +105,29 @@ class MaterialUpdateOperationReference(object):
         """
 
         self._material_id = material_id
+
+    @property
+    def interpolation_parameters(self):
+        """Gets the interpolation_parameters of this MaterialUpdateOperationReference.  # noqa: E501
+
+        Parameters taken into account when some material property values need to be interpolated. When this object is part of a request, the parameters will be taken into account during interpolation, while when it's part of the response, the parameters show the values which were used (if any) during the interpolation. Note that the values used during interpolation might differ from what is requested.   # noqa: E501
+
+        :return: The interpolation_parameters of this MaterialUpdateOperationReference.  # noqa: E501
+        :rtype: dict(str, object)
+        """
+        return self._interpolation_parameters
+
+    @interpolation_parameters.setter
+    def interpolation_parameters(self, interpolation_parameters):
+        """Sets the interpolation_parameters of this MaterialUpdateOperationReference.
+
+        Parameters taken into account when some material property values need to be interpolated. When this object is part of a request, the parameters will be taken into account during interpolation, while when it's part of the response, the parameters show the values which were used (if any) during the interpolation. Note that the values used during interpolation might differ from what is requested.   # noqa: E501
+
+        :param interpolation_parameters: The interpolation_parameters of this MaterialUpdateOperationReference.  # noqa: E501
+        :type: dict(str, object)
+        """
+
+        self._interpolation_parameters = interpolation_parameters
 
     def to_dict(self):
         """Returns the model properties as a dict"""
